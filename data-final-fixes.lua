1cf0b87784aa82a4b0bb24e566d05539e0a60434
iron-bound <Iron-Bound@users.noreply.github.com>
2025-06-17 21:13:50 +0200

    feat: mod kgo

---
+++ data-final-fixes.lua
@@ -0,0 +1,24 @@
+-- remove technology research
+data.raw.technology["logistics"].enabled = false
+data.raw.technology["logistics-2"].enabled = false
+data.raw.technology["logistics-3"].enabled = false
+data.raw.technology["turbo-transport-belt"].enabled = false
+
+-- disable items
+data.raw["recipe"]["transport-belt"].enabled = false
+data.raw["recipe"]["transport-belt"].hidden = true
+
+data.raw["recipe"]["underground-belt"].enabled = false
+data.raw["recipe"]["underground-belt"].hidden = true
+
+data.raw["recipe"]["splitter"].enabled = false
+data.raw["recipe"]["splitter"].hidden = true
+
+
+-- update science pack
+data.raw["recipe"]["logistic-science-pack"].ingredients =
+    {
+      {type = "item", name = "inserter", amount = 1},
+      {type = "item", name = "iron-plate", amount = 1},
+      {type = "item", name = "iron-gear-wheel", amount = 1}
+    }
