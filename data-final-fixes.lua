64d7ffe6a0519f7a5ec7260ccd8cadb700a1cb3e
Iron-Bound <Iron-Bound@users.noreply.github.com>
2025-06-20 00:09:14 +0200

    fix: double up

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -18,16 +18,15 @@ data.raw["recipe"]["splitter"].hidden = true
 -- update lab recipe
 data.raw["recipe"]["lab"].ingredients =
 {
-      {type = "item", name = "electronic-circuit", amount = 10},
-      {type = "item", name = "iron-gear-wheel", amount = 10},
-      {type = "item", name = "iron-plate", amount = 4},
-      {type = "item", name = "iron-gear-wheel", amount = 4}
+  { type = "item", name = "electronic-circuit", amount = 10 },
+  { type = "item", name = "iron-gear-wheel",    amount = 14 },
+  { type = "item", name = "iron-plate",         amount = 4 }
 }
 
 -- update science pack
 data.raw["recipe"]["logistic-science-pack"].ingredients =
 {
-  {type = "item", name = "inserter", amount = 1},
-  {type = "item", name = "iron-plate", amount = 1},
-  {type = "item", name = "iron-gear-wheel", amount = 1}
+  { type = "item", name = "inserter",        amount = 1 },
+  { type = "item", name = "iron-plate",      amount = 1 },
+  { type = "item", name = "iron-gear-wheel", amount = 1 }
 }
